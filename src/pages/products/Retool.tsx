27ef18dc40feef543c16cfd0d78d26a60637ba75
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:49:06 +0200

    Updated Retool.tsx

--- src/pages/products/Retool.tsx
+++ src/pages/products/Retool.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import retoolLogo from "../../assets/logos/retool.png";
 import {useEffect} from "react" 
 const Retool = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const Retool = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/Retool.png" alt="Retool" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={retoolLogo} alt="Retool" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Retool</h1>
         </div>
 
