77ee572239da1f31e938ecebd1f831471a6835ee
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:37:09 +0200

    Updated Airbyte.tsx

--- src/pages/products/Airbyte.tsx
+++ src/pages/products/Airbyte.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import airbyteLogo from "../../assets/logos/airbyte.png";
 import {useEffect} from "react" 
 const Airbyte = () => { 
   const location = useLocation();
@@ -24,7 +25,8 @@ const Airbyte = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/airbyte.png" alt="Airbyte" className="w-20 h-20 rounded-xl shadow-md" />
+         <img src={airbyteLogo} alt="Airbyte" className="w-20 h-20 rounded-xl shadow-md" />
+
           <h1 className="text-4xl font-sora font-bold">Airbyte</h1>
         </div>
 
