b9db98c3926af7a832b1797e501eaef4fd80729a
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:37:33 +0200

    Updated Airtable.tsx

--- src/pages/products/Airtable.tsx
+++ src/pages/products/Airtable.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import airtableLogo from "../../assets/logos/airtable.png";
 import {useEffect} from "react" 
 const Airtable = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const Airtable = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/airtable.png" alt="Airtable" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={airtableLogo} alt="Airtable" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Airtable</h1>
         </div>
 
