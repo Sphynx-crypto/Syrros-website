a33efd151c6e9adfa67e465f64ffbcd5dca53cb7
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:46:07 +0200

    Updated n8n.tsx

--- src/pages/products/n8n.tsx
+++ src/pages/products/n8n.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import n8nLogo from "../../assets/logos/n8n.png";
 import {useEffect} from "react" 
 const n8n = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const n8n = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/n8n.png" alt="n8n" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={n8nLogo} alt="n8n" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">n8n</h1>
         </div>
 
