d52272df0b2ec3356c9c2591fcdbcdfbbc818784
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:46:26 +0200

    Updated NotebookLM.tsx

--- src/pages/products/NotebookLM.tsx
+++ src/pages/products/NotebookLM.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import notebooklmLogo from "../../assets/logos/notebooklm.png"
 import {useEffect} from "react" 
 const NotebookLM = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const NotebookLM = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/notebooklm.png" alt="NotebookLM" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={notebooklmLogo} alt="NotebookLM" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">NotebookLM</h1>
         </div>
 
