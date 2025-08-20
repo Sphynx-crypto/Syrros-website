a0b1219e95ed7872dbec823d3050c9f7c47e12ed
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:52:09 +0200

    Updated Whalesync.tsx

--- src/pages/products/Whalesync.tsx
+++ src/pages/products/Whalesync.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import whalesyncLogo from "../../assets/logos/Whalesync.png";
 import {useEffect} from "react" 
 const Whalesync = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const Whalesync = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/Whalesync.png" alt="Whalesync" className="w-20 h-20 rounded-xl shadow-md" />
+         <img src={whalesyncLogo} alt="Whalesync" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Whalesync</h1>
         </div>
 
