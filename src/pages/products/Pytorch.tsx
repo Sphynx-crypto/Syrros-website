ccfa5ebebba51350955a0f0bc16fa17eb5d055ca
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:48:24 +0200

    Updated Pytorch.tsx

--- src/pages/products/Pytorch.tsx
+++ src/pages/products/Pytorch.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import pytorchLogo from "../../assets/logos/pytorch.png";
 import {useEffect} from "react" 
 const Pytorch = () => { 
   const location = useLocation();
@@ -24,7 +25,8 @@ const Pytorch = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/pytorch.png" alt="Pytorch" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={pytorchLogo} alt="Pytorch" className="w-20 h-20 rounded-xl shadow-md" />
+
           <h1 className="text-4xl font-sora font-bold">Pytorch</h1>
         </div>
 
