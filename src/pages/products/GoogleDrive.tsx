e380f1e5f12e6c834eaf89147ade427438f6b616
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:39:32 +0200

    Updated GoogleDrive.tsx

--- src/pages/products/GoogleDrive.tsx
+++ src/pages/products/GoogleDrive.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import googleDriveLogo from "../../assets/logos/google-drive.png";
 import {useEffect} from "react" 
 const GoogleDrive = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const GoogleDrive = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/google-drive.png" alt="Google Drive" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={googleDriveLogo} alt="Google Drive" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Google Drive</h1>
         </div>
 
