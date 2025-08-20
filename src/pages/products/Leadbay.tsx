d4e3ede210298c8d8c80692250a524be3fd6ef78
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:44:34 +0200

    Updated Leadbay.tsx

--- src/pages/products/Leadbay.tsx
+++ src/pages/products/Leadbay.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import leadbayLogo from "../../assets/logos/Leadbay.png";
 import {useEffect} from "react" 
 const Leadbay = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const Leadbay = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/Leadbay.png" alt="Leadbay" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={leadbayLogo} alt="Leadbay" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Leadbay</h1>
         </div>
 
