bdd5d4e9bd44da76b945b54690a6389e5b971d5e
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:44:55 +0200

    Updated Leexi.tsx

--- src/pages/products/Leexi.tsx
+++ src/pages/products/Leexi.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import leexiLogo from "../../assets/logos/Leexi.png";
 import {useEffect} from "react" 
 const Leexi = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const Leexi = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/Leexi.png" alt="Leexi" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={leexiLogo} alt="Leexi" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Leexi</h1>
         </div>
 
