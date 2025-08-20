c108a079c528bdf7b97a47956a6cc8c7c637ec94
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:50:59 +0200

    Updated Softr.tsx

--- src/pages/products/Softr.tsx
+++ src/pages/products/Softr.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import softrLogo from "../../assets/logos/softr.png";
 import {useEffect} from "react" 
 const Softr = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const Softr = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/softr.png" alt="Softr" className="w-20 h-20 rounded-xl shadow-md" />
+         <img src={softrLogo} alt="Softr" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Softr</h1>
         </div>
 
