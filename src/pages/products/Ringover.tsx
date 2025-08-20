537df117e3ba9670ab80a0a0d3104af2c608aa6c
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:49:53 +0200

    Updated Ringover.tsx

--- src/pages/products/Ringover.tsx
+++ src/pages/products/Ringover.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import ringoverLogo from "../../assets/logos/Ringover.png";
 import {useEffect} from "react" 
 const Ringover = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const Ringover = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/Ringover.png" alt="Ringover" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={ringoverLogo} alt="Ringover" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Ringover</h1>
         </div>
 
