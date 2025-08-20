6abbf15bf8a83257f39f9395a2946c39c8fa0d72
Sphynx-crypto <ethan.strohm2@gmail.com>
2025-08-20 14:39:49 +0200

    Updated Hubspot.tsx

--- src/pages/products/Hubspot.tsx
+++ src/pages/products/Hubspot.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import CallToActionBlock from "../../components/CallToActionBlock";
 import { ArrowLeft } from "lucide-react";
+import hubspotLogo from "../../assets/logos/Hubspot.png";
 import {useEffect} from "react" 
 const Hubspot = () => { 
   const location = useLocation();
@@ -24,7 +25,7 @@ const Hubspot = () => {
         <div className="px-16 py-12">
         {/* Titre et logo */}
         <div className="flex items-center gap-6 mb-10">
-          <img src="/src/assets/logos/Hubspot.png" alt="Hubspot" className="w-20 h-20 rounded-xl shadow-md" />
+          <img src={hubspotLogo} alt="Hubspot" className="w-20 h-20 rounded-xl shadow-md" />
           <h1 className="text-4xl font-sora font-bold">Hubspot</h1>
         </div>
 
